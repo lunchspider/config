2e881aba39a6ec7f03f16c8ec540fe91362075a2
lunchspider <lunchspider03@gmail.com>
2024-05-20 12:41:07 +0530

    added latex support in nvim

---
+++ nvim/lua/custom/plugins/tex.lua
@@ -0,0 +1,15 @@
+return {
+    "lervag/vimtex",
+    lazy = false,
+    tag = "v2.15", -- uncomment to pin to a specific release
+    init = function ()
+        vim.g['vimtex_view_method'] = 'zathura'     -- main variant with xdotool (requires X11; not compatible with wayland)
+        --vim.g['vimtex_view_method'] = 'zathura_simple' -- for variant without xdotool to avoid errors in wayland
+        vim.g['vimtex_quickfix_mode'] = 0              -- suppress error reporting on save and build
+        vim.g['vimtex_mappings_enabled'] = 0           -- Ignore mappings
+        vim.g['vimtex_indent_enabled'] = 0             -- Auto Indent
+        vim.g['tex_flavor'] = 'latex'                  -- how to read tex files
+        vim.g['tex_indent_items'] = 0                  -- turn off enumerate indent
+        vim.g['tex_indent_brace'] = 0                  -- turn off brace indent
+    end
+}

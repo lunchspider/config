9710fc826b564eeb15d0db9be9773fa26f7b2e6f
lunchspider <contact@aman-sh.com>
2024-10-26 17:37:19 +0530

    feat(neovim): show fullpath in statusline in neovim

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,5 +1,16 @@
 require('lualine').setup{
     options = {
         theme = 'rose-pine',
+    },
+    sections = {
+        lualine_c = {
+            {
+                'filename',
+                file_status = true,  -- displays file status (readonly status, modified status)
+                path = 1,            -- 0 = just filename, 1 = relative path, 2 = absolute path
+                shorting_target = 40 -- Shortens path to leave 40 space in the window
+                                   -- for other components. Terrible name any suggestions?
+            }
+        }
     }
 }
